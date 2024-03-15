adfbdbc895a4e266bbffc724f32d9d9b06b59595
Andreehahay <144866456+Andreehahay@users.noreply.github.com>
2024-03-15 23:06:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "Eyoooo kamu sangat amat cwantikk kyuttt nallaaa😘😘";
+    yesMessage.textContent = "Eyoooo kamu sangat amat cwantikk kyuttt kellaaa😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
